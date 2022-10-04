09cab8057e54288b34af63013850d8dd0cb8fdfb
loiabvn <trongloi-le@am-bition.vn>
2022-10-04 14:43:05 +0700

    imp#. update

--- tests/test_login.py
+++ tests/test_login.py
@@ -17,10 +17,15 @@ class TestLogin(BaseClass):
         self.driver.find_element(by=By.ID, value="submit").click()
         message = self.driver.find_element(by=By.XPATH, value='//*[@id="loop-container"]/div/article/div[1]/h1').text
         time.sleep(5)
+        self.driver.get_screenshot_as_file(filename="../reports/screenshot.png")
         assert message == 'Logged In Successfully'
 
     def test_tc2(self):
+        logger = self.getLogger()
+        logger.info("execute test_tc2")
         assert True
 
     def test_tc3(self):
+        logger = self.getLogger()
+        logger.info("execute test_tc3")
         assert True
--- utilities/BaseClass.py
+++ utilities/BaseClass.py
@@ -6,17 +6,16 @@ import pytest
 
 @pytest.mark.usefixtures('setup')
 class BaseClass:
-    pass
-    # def getLogger(self):
-    #     func_name = inspect.stack()[1][3]
-    #     logger = logging.getLogger(func_name)
-    #     file_handler = logging.FileHandler('debug.log')
-    #     formatter = logging.Formatter("%(asctime)s :%(levelname)s : %(name)s :%(message)s")
-    #     file_handler.setFormatter(formatter)
-    #
-    #     logger.addHandler(file_handler)  # filehandler object
-    #
-    #     logger.setLevel(logging.DEBUG)
-    #     return logger
+    def getLogger(self):
+        func_name = inspect.stack()[1][3]
+        logger = logging.getLogger(func_name)
+        file_handler = logging.FileHandler('debug.log')
+        formatter = logging.Formatter("%(asctime)s :%(levelname)s : %(name)s :%(message)s")
+        file_handler.setFormatter(formatter)
+
+        logger.addHandler(file_handler)  # filehandler object
+
+        logger.setLevel(logging.DEBUG)
+        return logger
 
 
